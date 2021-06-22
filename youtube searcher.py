d96dbba6503ff24966a575021d01d2adc2575611
SansUndertaleLab <70209030+SansUndertaleLab@users.noreply.github.com>
2021-06-22 20:44:27 +0800

    yes

--- youtube searcher.py
+++ youtube searcher.py
@@ -5,5 +5,5 @@ searchlink = "https://www.youtube.com/results?search_query="
 search=input().replace(" ","+")
 searchlink = "https://www.youtube.com/results?search_query="+search
 html = urllib.request.urlopen(searchlink)
-videoIds=re.findall(r"watch\?v=(\S{11})",html.read().decode())
+videoIds = re.findall(r"watch\?v=(\S{11})",html.read().decode())
 webbrowser.open("https://www.youtube.com/watch?v=" + videoIds[0])
