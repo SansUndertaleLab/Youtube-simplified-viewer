090da55017b87acd7a7fe7e19525c272e6e54530
SansUndertaleLab <70209030+SansUndertaleLab@users.noreply.github.com>
2021-06-23 10:47:49 +0800

    Delete youtube searcher.py

--- youtube searcher.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-import urllib.request
-import re
-searchlink = "https://www.youtube.com/results?search_query="
-search=input().replace(" ","+")
-searchlink = "https://www.youtube.com/results?search_query="+search
-html = urllib.request.urlopen(searchlink)
-videoIds = re.findall(r"watch\?v=(\S{11})",html.read().decode())
-webbrowser.open("https://www.youtube.com/watch?v=" + videoIds[0])
